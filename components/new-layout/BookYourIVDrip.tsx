cf61c22b94953f0da7b2c50c31f289007eb31ed5
pratishprince369 <pratishprince369@users.noreply.github.com>
2026-04-10 05:30:34 +0530

    Update Book Your IV Drip section: reduce iframe height to 650px

--- components/new-layout/BookYourIVDrip.tsx
+++ components/new-layout/BookYourIVDrip.tsx
@@ -31,7 +31,7 @@ export default function BookYourIVDrip() {
 
           <div className="bg-white p-8 lg:p-10 rounded-[2rem] shadow-xl border border-gray-100">
             <h3 className="text-2xl font-black text-[#020d1f] mb-8 uppercase tracking-wide">Book Your Session</h3>
-            <iframe width="100%" height="1000" src="https://ehr.charmtracker.com/publicCal.sas?method=getCal&digest=01389c02584758d12e92e9396b7eb77f180cec49f3ab362ab2267a8fddc4c6206ee57013c1a537e6aa69605fce4db66ad95fcbc3b76e32a2" style={{ overflow: 'hidden' }} frameBorder="0"></iframe>
+            <iframe width="100%" height="650" src="https://ehr.charmtracker.com/publicCal.sas?method=getCal&digest=01389c02584758d12e92e9396b7eb77f180cec49f3ab362ab2267a8fddc4c6206ee57013c1a537e6aa69605fce4db66ad95fcbc3b76e32a2" style={{ border: 'none' }} frameBorder="0"></iframe>
           </div>
         </div>
 
